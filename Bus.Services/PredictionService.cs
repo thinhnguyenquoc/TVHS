350c61c119cd06fd1488961556857eb6cb1233d2
Thinh Nguyen <nqt900@gmail.com>
2015-12-03 22:22:17 +0700

    dém

--- Bus.Services/PredictionService.cs
+++ Bus.Services/PredictionService.cs
@@ -13,22 +13,29 @@ namespace TVHS.Services
     {
         IProgramRepository _iProgramRepository;
         IScheduleRepository _iScheduleRepository;
-        public PredictionService(IProgramRepository iProgramRepository, IScheduleRepository iScheduleRepository)
-        {
+        ISaleRepository _iSaleRepository;
+        public PredictionService(IProgramRepository iProgramRepository, IScheduleRepository iScheduleRepository, ISaleRepository iSaleRepository)
+        { 
             _iProgramRepository = iProgramRepository;
             _iScheduleRepository = iScheduleRepository;
+            _iSaleRepository = iSaleRepository;
         }
         public int QuantityPredict(string programCode, int noTimes)
         {
             var program = _iProgramRepository.All.Where(x=>x.ProgramCode == programCode).FirstOrDefault();
-            //if (program != null)
-            //{
-            //    var programGroup = _iProgramRepository.All.Where(x => x.ProductId == program.ProductId).ToList();
-            //}
-            var scheduleDay = _iScheduleRepository.All.Where(x => x.ProgramCode == program.ProgramCode).GroupBy(x => EntityFunctions.TruncateTime(x.Date)).ToList().Where(x=>x.Count() == noTimes);
+            if (program != null)
+            {
+                var programGroup = _iProgramRepository.All.Where(x => x.ProductId == program.ProductId).ToList();
+            }
+            var scheduleDay = _iScheduleRepository.All.Where(x => x.ProgramCode == program.ProgramCode && x.Date.Year != 1).GroupBy(x => EntityFunctions.TruncateTime(x.Date)).ToList().Where(x=>x.Count() == noTimes).ToList();
+            if (scheduleDay.Count() == 0)
+            {
+                return -1;
+            }
+            var day = scheduleDay.OrderBy(x => x.Key).LastOrDefault().Key;
+            int quantity = _iSaleRepository.All.Where(x => x.ProductCode == program.ProductId && x.Date == day).FirstOrDefault().Quantity;
 
-
-            return scheduleDay.Count();
+            return quantity;
         }
     }
 }
